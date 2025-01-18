cb3640a7a4cf67b492a4b964ab01097973b112d8
jigneshmarvadi27 <jigneshmarvadi75@gmail.com>
2025-01-18 13:42:59 +0530

    Add files via upload

---
+++ exp.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const scrollContainers = [
+      { id: "roverImages", speed: 2 },
+      { id: "swarmRobotImages", speed: 2}
+    ];
+  
+    scrollContainers.forEach(({ id, speed }) => {
+      const scrollContainer = document.getElementById(id);
+  
+      // Duplicate images for seamless scrolling
+      const images = scrollContainer.innerHTML;
+      scrollContainer.innerHTML += images;
+  
+      let scrollPosition = 0;
+  
+      function scrollImages() {
+        scrollPosition -= speed; // Adjust scroll speed
+        if (Math.abs(scrollPosition) >= scrollContainer.scrollWidth / 2) {
+          scrollPosition = 0; // Reset scroll position
+        }
+        scrollContainer.style.transform = `translateX(${scrollPosition}px)`;
+        requestAnimationFrame(scrollImages);
+      }
+  
+      scrollImages(); // Start scrolling
+    });
+  });
